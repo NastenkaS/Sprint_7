96503318122a6edf452a05a5705fbcc7ec92a204
Anastazie Skrabec <anastassiya.petukhova@gmail.com>
2025-12-20 18:02:34 +0100

    Improved tests and added missing classes

---
+++ src/main/java/yandex/model/CourierCredentials.java
@@ -0,0 +1,13 @@
+package yandex.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourierCredentials {
+    private String login;
+    private String password;
+}
